6501ae59a952d0c049b94cd104e92c5ed730da47
Josh <joshsarnecki@gmail.com>
2022-07-25 16:02:07 -0700

    Updates font family to 'Open Sans' and font-weight to have a bold.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,12 +10,15 @@
         <div class="col-3">
             <img class="first" alt="first image added" src="https://external-preview.redd.it/QJR9EFbelLAg4a8DfHwFF7PJO1lIFWkda9mB81WJUow.jpg?width=640&crop=smart&auto=webp&s=baf5ef7ccdd714fb30ff8255360fff5286b809f3">
         </div>
-        <div class="col-9">
+        <div class="description-box col-9">
             <div><h1>Insta-Clone</h1></div>
+            <div class="count-description">
+                <div class="count-description-item"><strong>123</strong> posts</div>
+                <div class="count-description-item"><strong>43k</strong> followers</div>
+                <div class="count-description-item"><strong>212</strong> following</div>
+            </div>
         </div>
-        <p class="test">
-            THIS IS A TEST
-        </p>
+
     </div>
 </div>
 @endsection
