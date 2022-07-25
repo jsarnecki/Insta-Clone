80bfd84b3cbd5f9b8a9fbf77c003edc898705845
Josh <joshsarnecki@gmail.com>
2022-07-25 15:37:57 -0700

    Css compiling issue sorted.  It needed the css reference inside app.blade.php.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,7 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/navbar.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/homepage.css') }}" rel="stylesheet">
 
 </head>
 <body>
