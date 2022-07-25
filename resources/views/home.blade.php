0aa71aea6d32ba00878d8db166a594902bd91c1c
Josh <joshsarnecki@gmail.com>
2022-07-25 16:45:44 -0700

    Adds images for dashboard.  Uses object-fit: cover; to have imgs of varying sizes fill to the same size.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,8 +18,20 @@
                 <div class="count-description-item"><strong>212</strong> following</div>
             </div>
             <div class="pt-4"><strong>website name here</strong></div>
-            <div> lorem ipsum description for account here </div>
+            <div> lorem ipsum description for account here.. and here let me just type some more random shit to make this even longer than is necessary okay great </div>
             <div><a href="#">link to website here</a></div>
+        </div>
+
+        <div class="row image-container">
+            <div class="col-4">
+               <img class="dashboard-img" src="https://i.pinimg.com/originals/cc/57/a7/cc57a7094af5c1f4abfcd44808ebaf28.jpg">
+            </div>
+            <div class="col-4">
+                <img class="dashboard-img" src="https://i.pinimg.com/736x/9e/c0/ba/9ec0ba556cc68b9c51801cb08f338039--doona-bae.jpg">
+            </div>
+            <div class="col-4">
+                <img class="dashboard-img" src="https://nsbb.in/wp-content/uploads/2022/03/Bae-Doona-Biography-Actress-Wiki-Age-Boyfriend-Family-Facts-810x731.jpg">
+            </div>
         </div>
 
     </div>
