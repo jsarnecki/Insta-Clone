8bace8b6503c0796851888f0e403461b2599e1f2
Josh <joshsarnecki@gmail.com>
2022-07-25 14:33:00 -0700

    Creates migration files, authentication for the database.  Registered a test user, tested authentication.  Brings in logo for the nav, adjusted css.  Brings in first image to play around with.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,12 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="row">
+        <div class="col-3">
+            <img class="rounded-circle" src="https://external-preview.redd.it/QJR9EFbelLAg4a8DfHwFF7PJO1lIFWkda9mB81WJUow.jpg?width=640&crop=smart&auto=webp&s=baf5ef7ccdd714fb30ff8255360fff5286b809f3">
+        </div>
+        <div class="col-9">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
         </div>
     </div>
 </div>
