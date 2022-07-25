9ed787fdd792c3dcd0bd7eb50de4d122568cdc3f
Josh <joshsarnecki@gmail.com>
2022-07-25 15:03:42 -0700

    home.blade.php is able to use navbar.css when referred to it, but when referring to homepage.css is unable to load the css.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,21 @@
 @extends('layouts.app')
 
+@push('styles')
+    <link href="{{ asset('css/homepage.css') }}" rel="stylesheet">
+@endpush
+
 @section('content')
 <div class="container">
     <div class="row">
         <div class="col-3">
-            <img class="rounded-circle" src="https://external-preview.redd.it/QJR9EFbelLAg4a8DfHwFF7PJO1lIFWkda9mB81WJUow.jpg?width=640&crop=smart&auto=webp&s=baf5ef7ccdd714fb30ff8255360fff5286b809f3">
+            <img class="first" src="https://external-preview.redd.it/QJR9EFbelLAg4a8DfHwFF7PJO1lIFWkda9mB81WJUow.jpg?width=640&crop=smart&auto=webp&s=baf5ef7ccdd714fb30ff8255360fff5286b809f3">
         </div>
         <div class="col-9">
 
         </div>
+        <p class="test">
+            THIS IS A TEST
+        </p>
     </div>
 </div>
 @endsection
