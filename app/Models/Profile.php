c3398801a068af1e76835ed0840e75a56566c1d5
Josh <joshsarnecki@gmail.com>
2022-07-26 11:15:18 -0700

    Establishes one to one relationship between Profile and User

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Profile extends Model
 {
     use HasFactory;
+
+    public function user() // This naming convention of having this func be the same name as the model (it can be overridden)
+    {
+        return $this->belongsTo(User::class); // Shows 1-1 relationship between Profile+User
+        // You can visit belongsTo func to see that 2nd and 3rd param allow for different naming conventions if needed
+    }
 }
