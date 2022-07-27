0c690d55bef0f6a515196c24356f6cbd1f9721bb
Josh <joshsarnecki@gmail.com>
2022-07-27 09:42:21 -0700

    Sets up 'add new post' page frontend, some minor CSS changes still needed

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,24 +2,48 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-8 offset-2">
+   <form action="/p" enctype="multipart/form-data" method="POST">
+       <div class="row">
+           <div class="col-8 offset-2">
 
-            <div class="row mb-3">
-                <label for="caption" class="col-md-4 col-form-label text-md-end">Post Caption</label>
+               <div class="row">
+                   <h2>Add New Post</h2>
+               </div>
 
-                <div class="col-md-6">
-                    <input id="caption" type="text" class="form-control @error('caption') is-invalid @enderror" name="caption" value="{{ old('caption') }}" autocomplete="caption" autofocus>
+               <div class="form-group row">
+                   <label for="caption" class="col-md-4 col-form-label">Post Caption</label>
+                   <input id="caption"
+                          type="text"
+                          class="form-control
+                           @error('caption') is-invalid @enderror"
+                          name="caption" value="{{ old('caption') }}"
+                          autocomplete="caption" autofocus>
 
-                    @error('caption')
+                   @error('caption')
+                       <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                   @enderror
+
+               </div>
+           </div>
+
+           <div class="row">
+               <label for="image" class="col-md-4 col-form-label">Post Image</label>
+               <input type="file" class="form-control-file" id="image" name="image">
+
+               @error('image')
                     <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                    @enderror
-                </div>
-            </div>
+                        <strong>{{ $message }}</strong>
+                    </span>
+               @enderror
+           </div>
 
-        </div>
-    </div>
+           <div class="row">
+               <button class="btn btn-primary">Add New Post</button>
+           </div>
+
+       </div>
+   </form>
 </div>
 @endsection
